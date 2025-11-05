719fbe39d67e8aca42f6123da2c3848426b964ac
offyerrocker <offyerrocker88@gmail.com>
2025-11-04 23:15:19 -0800

    add anim keys

--- lua/fpcameraplayerbase.lua
+++ lua/fpcameraplayerbase.lua
@@ -1,3 +1,24 @@
+function FPCameraPlayerBase:anim_execution_slap(...)
+
+end
+function FPCameraPlayerBase:anim_execution_punchthroat(...)
+
+end
+function FPCameraPlayerBase:anim_execution_grab(...)
+
+end
+function FPCameraPlayerBase:anim_execution_kill(...)
+	local player = managers.player:local_player()
+	local mov_ext = alive(player) and player:movement()
+	local state = mov_ext and mov_ext:current_state()
+	if state then 
+		if alive(state._state_data.execution_unit) then
+			--state._state_data.execution_unit:anim_data().ragdoll = true
+		end
+		mov_ext:change_state("standard")
+	end
+end
+
 function FPCameraPlayerBase:anim_start_execution(...)
 	Print("FPCameraPlayerBase:anim_start_execution()")
 --	managers.player:local_player():movement():change_state("standard")
@@ -7,3 +28,5 @@ function FPCameraPlayerBase:anim_stop_execution(...)
 	Print("FPCameraPlayerBase:anim_stop_execution()")
 	managers.player:local_player():movement():change_state("standard")
 end
+
+--]]
