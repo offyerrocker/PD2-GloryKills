8760351417bdd0bf672cc959aedde266ca0feee3
offyerrocker <offyerrocker88@gmail.com>
2025-11-10 22:30:01 -0800

    cleaning

--- lua/fpcameraplayerbase.lua
+++ lua/fpcameraplayerbase.lua
@@ -15,9 +15,6 @@ function FPCameraPlayerBase:anim_execution_kill(...)
 	local mov_ext = alive(player) and player:movement()
 	local state = mov_ext and mov_ext:current_state()
 	if state then 
-		if alive(state._state_data.execution_unit) then
-			--state._state_data.execution_unit:anim_data().ragdoll = true
-		end
 		mov_ext:change_state("standard")
 	end
 end
